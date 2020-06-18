92c8a82aad3d0e3b6cfa003306a318b01556f7ce
Technologic22 <biomuhammet@gmail.com>
2020-06-18 03:43:57 -0400

    Interface BASICS

--- src/cybertekschool/day48_AbstractClass_Interface/Bouncible.java
+++ src/cybertekschool/day48_AbstractClass_Interface/Bouncible.java
@@ -2,7 +2,17 @@ package cybertekschool.day48_AbstractClass_Interface;
 
 public interface Bouncible {
 
+    //--ALL THE MEMBERS IN INTERFACE MUST BE PUBLIC
+    //--THIS IS EXCLUSIVE TO INTERFACE  ==  NOWHERE ELSE U GET THIS AT ALL!!!!!!
+    //--if we dont add add any part of "public static final" for the field
+    //-- all interface fields are automatically public static final
     public static final double GRAVITY=9.81;
+
+    //-- Every memeber of Interface is automatically 'PUBLIC'
+    //-- If no access modifier is given
+    //--all the method with no body automatically public abstract
+
+    //void bounce();
 
     public abstract void bounce();
 
