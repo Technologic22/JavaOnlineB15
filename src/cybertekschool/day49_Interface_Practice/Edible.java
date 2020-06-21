2cd522d2b3712664148f1db82bedec7f3be1ffa5
Technologic22 <biomuhammet@gmail.com>
2020-06-21 04:22:34 -0400

    iNTERFACE default Method/Keyword

--- src/cybertekschool/day49_Interface_Practice/Edible.java
+++ src/cybertekschool/day49_Interface_Practice/Edible.java
@@ -11,5 +11,15 @@ public interface Edible {
     //--all methods are 'PUBLIC ABSTRACT' if not defined explicitly
     void eat();
     public abstract void drink();
-    //public abstract void digest();
+
+    //--!! when U add new method to Interface, all the implementing classes will start to fail
+
+    //--since J 8, default implementation to not the break current implementations
+    //--classes have option to use it as is
+    //--override it according to their own requirement
+    //--ONLY EXIST IN INTERFACE
+    //-- 'DEFAULT' Keyword NOT A ACCESS MODIFIER
+    public default void digest(){
+        System.out.println("U implement the code yourself");
+    }
 }
--- src/cybertekschool/day49_Interface_Practice/IceCream.java
+++ src/cybertekschool/day49_Interface_Practice/IceCream.java
@@ -6,6 +6,26 @@ public class IceCream extends Object implements Edible{
 //--Who IS THE SUPERCLASS of IceCream class??
     //-- OBJECT CLASS
 
+    public static void main(String[] args) {
+
+        IceCream i1=new IceCream();
+
+        Object i2=new IceCream();   // generalization! Since OBJ class exists we can create Object of it
+        i1.drink();
+        i1.eat();
+        System.out.println(i1.toString());
+        i1.digest();
+
+        //--Since Object class exists we can create object out of it
+        //--Object o1= new IceCream();
+
+    }
+
+    @Override
+    public void digest(){
+        System.out.println("Digesting burger by chewing more!!");
+    }
+
     @Override
     public void eat() {
         System.out.println("Eat with spoon!!!");
@@ -16,13 +36,5 @@ public class IceCream extends Object implements Edible{
         System.out.println("drink it if it is melted!");
     }
 
-    public static void main(String[] args) {
 
-        IceCream i1=new IceCream();
-
-        Object i2=new IceCream();   // generalization! Since OBJ class exists we can create Object of it
-        i1.drink();
-        i1.eat();
-        System.out.println(i1.toString());
-    }
 }
