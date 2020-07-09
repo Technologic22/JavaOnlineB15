5a6023cc7151629653466ad085513f70cc6c033f
Technologic22 <biomuhammet@gmail.com>
2020-07-09 07:13:58 -0400

    comparing Custom Type

---
+++ src/cybertekschool/day51_polymorphism/PersonComparing.java
@@ -0,0 +1,12 @@
+package cybertekschool.day51_polymorphism;
+
+public class PersonComparing {
+
+    public static void main(String[] args) {
+
+        Person p1=new Person("Leyla", 3);
+        Person p2=new Person("Aynur", 7);
+
+
+    }
+}
