2d139e7b3a0f441dde60bfa5734d3c8ad2842392
Technologic22 <biomuhammet@gmail.com>
2021-09-22 18:30:39 -0400

    Polymorphism usage in Selenium

--- src/cybertekschool/day54_Polymorphism_practice_Interface_as_ref_type/Automation.java
+++ src/cybertekschool/day54_Polymorphism_practice_Interface_as_ref_type/Automation.java
@@ -6,10 +6,17 @@ public class Automation {
 
         WebDriver c1 = new ChromeDriver();
         ChromeDriver c2 = new ChromeDriver();
-
+        System.out.println("-----Running on Chrome-----");
                                             // this is a polymorphism,
                                             // Chromedriver is an implementing class,
                                                // Webdriover is an interface
+        c1.openBrowser();
+        c1.navigateTo("www.cybertekschool.com");
+        c1.maximize();
+        c1.closeBrowser();
+
+        System.out.println("-----Running on Firefox-----");
+        c1 = new FirefoxDriver();
         c1.openBrowser();
         c1.navigateTo("www.cybertekschool.com");
         c1.maximize();
