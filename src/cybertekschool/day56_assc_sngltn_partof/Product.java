fec839c2ebf9cee4077c7e9ac7fce243182d24b2
Technologic22 <biomuhammet@gmail.com>
2021-10-11 16:58:49 -0400

    day 56: Product: POJO - plain old java Object

---
+++ src/cybertekschool/day56_assc_sngltn_partof/Product.java
@@ -0,0 +1,41 @@
+package cybertekschool.day56_assc_sngltn_partof;
+
+public class Product {
+
+    private String name;
+    private int price;
+
+    public Product(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
