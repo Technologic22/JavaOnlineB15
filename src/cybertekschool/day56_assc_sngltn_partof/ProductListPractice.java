1a7934f67feec84121321a9543a05ec86715410b
Technologic22 <biomuhammet@gmail.com>
2021-10-14 15:52:23 -0400

    day 56: ProductList practice | contains, remove (decided by equals method internally!)

--- src/cybertekschool/day56_assc_sngltn_partof/ProductListPractice.java
+++ src/cybertekschool/day56_assc_sngltn_partof/ProductListPractice.java
@@ -3,6 +3,16 @@ package cybertekschool.day56_assc_sngltn_partof;
 import java.util.ArrayList;
 import java.util.List;
 
+// How does contains decide you have an object you are looking for in your list ?
+// it will call .equals method of that type , if it's overridden to check for content
+// then it will return desired result according to the content (attributes)
+// else it will always return false
+
+// same thing for remove ? how can i remove an item from the list using remove method that accept object ?
+// first it decide whether you have that object or not by checking using contain method .
+// if you have it then you remove ,  if contains return false you can not remove because we cant remove what we don't have
+
+
 public class ProductListPractice {
 
     public static void main(String[] args) {
@@ -18,6 +28,25 @@ public class ProductListPractice {
         productList.add(new Product("Sumsung Note", 1288));
 
         System.out.println("productList.size() = " + productList.size());
+
+        // How does contains method decide whether the list contains the object we are looking for ?
+        // it will internally call the equals method of that type (Product here)
+        // if it return true then contains method will return true if not , return false
+
+        //check whether we have "sony tv' with 499 price
+//       boolean result = false;
+//        for (Product each: productList) {
+////            if (each.getName().equals("Sony TV") && each.getPrice()==499){
+////                result = true;
+////            }
+//
+//            if (each.equals(new Product("Sony TV", 499))){
+//                result = true;
+//            }
+//         }
+//
+        boolean result = productList.contains(new Product("Sony TV", 499));
+//        System.out.println("Has sony Tv with 499 price or not result = " + result);
 
     }
 
